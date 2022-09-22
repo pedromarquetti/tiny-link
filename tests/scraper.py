bc1f80083980b18ac8683b1e4660cb41eb78de9a
pedromarquetti <pedro.marquettti@gmail.com>
2022-09-22 11:31:01 -0300

    updated scraper.py

--- tests/scraper.py
+++ tests/scraper.py
@@ -1,12 +1,21 @@
 import requests as req
-post = req.request(
-    "POST",
-    r"http://localhost:8080",
-    data={
-        "url": "a.e"
-    },
 
 
-)
-print(post.text)
-print(post.status_code)
+def main():
+    try:
+        post = req.request(
+            "POST",
+            r"http://localhost:8080",
+            data={
+                "url": "a.e"
+            },
+        )
+
+        print(post.text)
+        print(post.status_code)
+    except Exception as e:
+        print(f"{e} occurred. Exiting")
+
+
+if __name__ == '__main__':
+    main()
