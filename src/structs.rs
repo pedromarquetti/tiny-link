efae1875427ef847ba98d0579228ab02cf794207
pedromarquetti <pedro.marquettti@gmail.com>
2022-10-05 18:50:31 -0300

    updated structs

--- src/structs.rs
+++ src/structs.rs
@@ -1,3 +1,13 @@
+use crate::schema::tiny_link;
+
+#[derive(Queryable, Serialize, Debug)]
 pub struct LongUrl {
     pub long_url: String,
+}
+
+#[derive(Insertable, Debug)]
+#[table_name = "tiny_link"]
+pub struct TinyLink {
+    pub long_link: String,
+    pub short_link: String,
 }
