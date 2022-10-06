4d1eb03ac09456927e3fbd8281b3e6ee4a610ddb
pedromarquetti <pedro.marquettti@gmail.com>
2022-10-06 11:19:32 -0300

    diesel now requires PgConnection to be mutable

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,4 @@
-use crate::schema::tiny_link;
+use crate::schema::tiny_link::short_link;
 use crate::structs::{LongUrl, TinyLink};
 
 use diesel::prelude::*;
@@ -10,20 +10,21 @@ use std::io;
 /// writes received long URL to db, returns short Url that will be echoed to user
 pub fn write_to_db(
     recvd_long_url: String,
-    db_connection: &PgConnection,
+    db_connection: &mut PgConnection,
 ) -> FutureResult<String, Error> {
     // TODO:
     // 1. try to save recvd_long_url + short version
     // 2. return error if failed
     // 3. return short url
+    use crate::schema::tiny_link;
 
     let result: Result<String, db_err> = diesel::insert_into(tiny_link::table)
-        // .values(&recvd_long_url)
+        // inserting TinyLink with long + short url
         .values(&TinyLink {
             long_link: recvd_long_url,
             short_link: "teste".to_string(), // this has to be a short (6) random id
         })
-        .returning(tiny_link::short_link)
+        .returning(tiny_link::short_link) // returns short url to user
         .get_result(db_connection);
     match result {
         Ok(shortened) => futures::future::ok(shortened), // all ok, sending back short url
@@ -39,6 +40,10 @@ pub fn write_to_db(
     }
 }
 
-pub fn read_from_db(path: &str) -> Option<&str> {
-    Some(path)
+pub fn read_from_db(path: &str, db_connection: &PgConnection) -> Option<LongUrl> {
+    use crate::schema::tiny_link;
+
+    Some(LongUrl {
+        long_url: path.to_string(),
+    })
 }
