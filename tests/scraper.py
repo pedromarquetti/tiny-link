dd2b271853ab6633fcd9adc5e58cccfa0977704c
pedromarquetti <pedro.marquettti@gmail.com>
2022-10-07 09:39:27 -0300

    fixed test script

--- tests/scraper.py
+++ tests/scraper.py
@@ -1,24 +1,30 @@
 import requests as req
 
 
+def send_post(url: str):
+    return req.request(
+        "POST",
+        r"http://localhost:8080",
+        data={
+            "url": "http://ti.jolo"
+        },
+    )
+
+
+def send_get(path: str):
+    return req.request(
+        "GET",
+        f"http://localhost:8080/{path}",
+    )
+
+
 def main():
     try:
-        post = req.request(
-            "POST",
-            r"http://localhost:8080",
-            data={
-                "url": "a.e"
-            },
-        )
+        print("sending get req")
+        print(send_post("example.com").text)
+        print("sending get req... ")
+        print(send_get("teste").text)
 
-        print(post.text)
-        print(post.status_code)
-        get = req.request(
-            "GET",
-            r"http://localhost:8080",
-
-        )
-        print(get.text)
     except Exception as e:
         print(f"{e} occurred. Exiting")
 
