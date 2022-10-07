f234769854e031899432412ee5dbb607e55f34ab
pedromarquetti <pedro.marquettti@gmail.com>
2022-10-07 09:50:51 -0300

    Cleaned up post_response()
    Fixed get_response()

--- src/response.rs
+++ src/response.rs
@@ -1,4 +1,7 @@
-use crate::{error::make_error_response, structs::LongUrl};
+use crate::{
+    error::make_error_response,
+    structs::{LongUrl, ShortUrl, TinyLink},
+};
 use futures::future::FutureResult;
 use hyper::{
     header::{ContentLength, ContentType},
@@ -9,7 +12,9 @@ use hyper::{
 
 use serde_json::json;
 
-/// Send back response to user
+/// Send back Short URL to user
+///
+/// If Url parse failed returns make_error_response()
 pub fn post_response(res: Result<String, Error>) -> FutureResult<Response, Error> {
     match res {
         Ok(success_result) => {
@@ -24,32 +29,28 @@ pub fn post_response(res: Result<String, Error>) -> FutureResult<Response, Error
         }
         Err(error) => {
             let msg: String = error.to_string();
-
-            match msg.as_str() {
-                "Missing URL"
-                | "No empty strings or spaces!"
-                | "Could not parse URL, relative URL without a base"
-                | "Could not parse URL, empty host"
-                | "Could not parse URL, invalid domain character"
-                | "Could not parse URL, invalid IPv4 address" => {
-                    make_error_response(&msg, BadRequest)
-                }
-                err => make_error_response(
-                    format!("Internal server Error! {}", err).as_str(),
-                    InternalServerError,
-                ),
-            }
+            make_error_response(msg.as_str(), BadRequest)
         }
     }
 }
 
-/// Sends GET response with short
-pub fn get_response(path: &str) -> FutureResult<Response, Error> {
-    let response: Response = Response::new()
-        // .with_header(ContentLength(payload.len() as u64))
-        // .with_header(ContentType::json())
-        .with_body(path.to_owned());
-    info!("Success Response sent: {:?}", response);
+/// Sends GET response with Long URL
+pub fn get_response(path: Option<TinyLink>) -> FutureResult<Response, Error> {
+    match path {
+        Some(path) => {
+            let payload: String = json!(
+                { "short_link": path.short_link,"long_link":path.long_link }
+            )
+            .to_string();
 
-    futures::future::ok(response) // sending response
+            let response: Response = Response::new()
+                .with_header(ContentLength(payload.len() as u64))
+                .with_header(ContentType::json())
+                .with_body(payload);
+            info!("Success Response sent: {:?}", response);
+
+            futures::future::ok(response) // sending response
+        }
+        None => make_error_response("Fail on get_response()", InternalServerError),
+    }
 }
