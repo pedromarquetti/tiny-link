029f5f407fa1c5ab929f1b9cab36e3e96da64aac
pedromarquetti <pedro.marquettti@gmail.com>
2022-10-07 19:12:35 -0300

    updated structs

--- src/structs.rs
+++ src/structs.rs
@@ -4,8 +4,11 @@ use crate::schema::tiny_link;
 pub struct LongUrl {
     pub long_url: String,
 }
-
-#[derive(Insertable, Debug)]
+#[derive(Queryable, Serialize, Debug)]
+pub struct ShortUrl {
+    pub short_url: String,
+}
+#[derive(Queryable, Insertable, Debug)]
 #[table_name = "tiny_link"]
 pub struct TinyLink {
     pub long_link: String,
