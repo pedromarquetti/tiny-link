72211630e2e0fcd8647356dfe76db695872c3257
pedromarquetti <pedro.marquettti@gmail.com>
2022-10-08 20:55:11 -0300

    removed unusd comment from sql

--- schemas/tiny_link.sql
+++ schemas/tiny_link.sql
@@ -2,4 +2,4 @@ CREATE TABLE tiny_link (
     id SERIAL PRIMARY KEY,
     long_link TEXT NOT NULL,
     short_link VARCHAR(6) NOT NULL 
-)  -- tut https://www.cherryservers.com/blog/how-to-install-and-setup-postgresql-server-on-ubuntu-20-04
+)
