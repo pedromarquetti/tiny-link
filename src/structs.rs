f147315877e6dd105df4963f4db5531e17ddcaae
pedromarquetti <pedro.marquettti@gmail.com>
2022-10-08 19:41:55 -0300

    updated structs file

--- src/structs.rs
+++ src/structs.rs
@@ -9,7 +9,7 @@ pub struct ShortUrl {
     pub short_url: String,
 }
 #[derive(Queryable, Insertable, Debug)]
-#[table_name = "tiny_link"]
+#[diesel(table_name = tiny_link)]
 pub struct TinyLink {
     pub long_link: String,
     pub short_link: String,
