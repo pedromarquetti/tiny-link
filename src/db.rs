d92d8b9c11a42efda6d3117e53e3191f24f4a4ca
pedromarquetti <pedro.marquettti@gmail.com>
2023-04-01 22:04:09 -0300

    removed unused comments

--- src/db.rs
+++ src/db.rs
@@ -6,15 +6,12 @@ use diesel::{
     pg::PgConnection,
     r2d2::{ConnectionManager, Pool as R2D2Pool},
 };
-use r2d2::Error;
-use warp::Rejection;
 
 pub type R2D2Err = r2d2::Error;
 pub type Pool = R2D2Pool<ConnectionManager<PgConnection>>;
 pub type DbConnection = PooledConnection<ConnectionManager<PgConnection>>;
 // pub type DbConnection = Result<PooledConnection<ConnectionManager<PgConnection>>, R2D2Err>;
 
-use crate::error::convert_to_rejection;
 use crate::schema::tiny_link;
 
 #[derive(Queryable, Serialize, Debug, Deserialize)]
