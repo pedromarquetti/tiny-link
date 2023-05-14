59c6998ecb1656b9a25121fd8d76f63df9f7408b
pedromarquetti <pedro.marquettti@gmail.com>
2023-05-13 23:21:40 -0300

    removed unnecessary code

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -15,19 +15,14 @@ pub async fn create_user(user: User, conn: DbConnection) -> Result<impl Reply, R
 
     let mut conn = conn.map_err(convert_to_rejection)?;
 
-    let payload: User = User {
-        user_name: user.user_name,
-        user_role: user.user_role,
-        user_pwd: user.user_pwd,
-    };
     diesel::insert_into(users::table)
-        .values::<&User>(&payload)
+        .values::<&User>(&user)
         .returning(users::user_name)
         .execute(&mut conn)
         .map_err(convert_to_rejection)?;
     Ok(warp::reply::with_status(
         warp::reply::json(&json!({
-            "success": format!("User {} created!", payload.user_name)
+            "success": format!("User {} created!", user.user_name)
         })),
         StatusCode::CREATED,
     ))
