f1b17ac8e3f3848e993be5cfeb1109963b872f11
PedroMarquetti <pedro.marquettti@gmail.com>
2023-05-26 20:00:44 -0300

    temporary user_role valitaion

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -7,11 +7,19 @@ use warp::{Rejection, Reply};
 
 use crate::{
     db::{DbConnection, User},
-    error::convert_to_rejection,
+    error::{convert_to_rejection, Error},
 };
 
 pub async fn create_user(user: User, conn: DbConnection) -> Result<impl Reply, Rejection> {
     use crate::schema::users;
+
+    // temp impl for UserRole validation
+    if user.user_role != "adm" || user.user_role != "usr" {
+        return Err(convert_to_rejection(Error::invalid_forms(format!(
+            "'{}' is not a valid role! role has to be adm or usr",
+            user.user_role
+        ))));
+    }
 
     let mut conn = conn.map_err(convert_to_rejection)?;
 
